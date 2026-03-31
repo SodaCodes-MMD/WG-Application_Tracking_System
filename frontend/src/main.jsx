7f4f46a52c481bbe8a348f96a257a3510229c687
SodaCodes-MMD <sodamik3y@gmail.com>
2026-03-31 01:22:12 -0500

    fix: split ThemeContext to satisfy react-refresh/only-export-components lint rule

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider } from './context/ThemeContext.jsx'
+import ThemeProvider from './context/ThemeProvider.jsx'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
